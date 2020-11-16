fe727f40b0ee07dc00af5c05bb1152958c3a6943
zhangzp <zhangzp@goodcol.com>
2020-11-16 20:18:38 +0800

    2525

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,57 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around',
-    theme: 'reco',
-    themeConfig: {
-        // 博客配置
-        blogConfig: {
-        category: {
-            location: 2,     // 在导航栏菜单中所占的位置，默认2
-            text: 'Category' // 默认文案 “分类”
-        },
-        tag: {
-            location: 3,     // 在导航栏菜单中所占的位置，默认3
-            text: 'Tag'      // 默认文案 “标签”
-        }
-        }
-    }
+  title: 'kusch的小破站',
+  description: 'JUST FOR FUN',
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+    ['link', { rel: 'icon', href: '/images/photo.jpg' }],
+    ['link', { rel: 'manifest', href: '/images/photo.jpg' }],
+    ['link', { rel: 'apple-touch-icon', href: '/images/photo.jpg' }],
+    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache'}],
+    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache,must-revalidate'}],
+    ['meta', { 'http-quiv': 'expires', cotent: '0'}]
+  ],
+  serviceWorker: true, // 是否开启 PWA
+  base: '/', // 部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块是否显示行号
+  },
+  themeConfig: {
+    nav:[ // 导航栏配置
+      {text: '前端相关', link: '/accumulate/' },
+      {text: '算法相关', link: '/algorithm/'},
+      {text: 'java基础', link: '/java/'},
+      {text: '诗和远方', link: '/others/'},
+      {text: '其他', link: 'https://www.superkusch.fun'}      
+    ],
+    // sidebar:{
+    //   '/accumulate/': [
+    //       {
+    //         title: '前端积累',
+    //         children: [
+    //           '/accumulate/1.html',
+    //           '/accumulate/2.html',
+    //           '/accumulate/3.html',
+    //           '/accumulate/4.html',
+    //           '/accumulate/5.html',
+    //           '/accumulate/6.html',
+    //           '/accumulate/7.html',
+    //           '/accumulate/8.html',
+    //           '/accumulate/9.html',
+    //           '/accumulate/10.html',
+    //           '/accumulate/11.html',
+    //         ]
+    //       }
+    //     ],
+    //     '/algorithm/': [
+    //       '/algorithm/', 
+    //       {
+    //         title: '第二组侧边栏下拉框的标题1',
+    //         children: [
+    //           '/algorithm/' 
+    //         ]
+    //       }
+    //     ]
+    // },
+    sidebar: 'auto', // 侧边栏配置
+    sidebarDepth: 2
   }
+};
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,14 @@
 ---
 home: true
-heroImage: /hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
-actionLink: /zh/guide/
+heroImage: /images/photo.jpg
+actionText: 快速开始 →
+actionLink: guide.html
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 随心随缘的前端学习
+  details: 身为一名后端程序员，时不时来点前端学习，也充满乐趣。
+- title: 循序渐进的后端学习
+  details: 慢慢走，一步一个脚印。
+- title: 千锤百炼的基础学习
+  details: 基础总是最重要的，特别是算法！
+footer: 君不见黄河之水天上来，奔流到海不复回
 ---
