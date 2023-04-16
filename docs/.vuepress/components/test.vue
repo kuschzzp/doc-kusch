b9966d20503f8c83bf9997a57dd9479ed1762d52
zhangzp <zhangzp@goodcol.com>
2023-04-16 16:29:23 +0800

    放个页面

--- docs/.vuepress/components/test.vue
+++ docs/.vuepress/components/test.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
-    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="视频分享链接" prop="videoUrl">
-        <el-input type="text" v-model="ruleForm.videoUrl" autocomplete="off"></el-input>
-      </el-form-item>
-
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <iframe src="https://chat.superkusch.fun/" width="1000" height="800"></iframe>
   </div>
 </template>
 <script>
