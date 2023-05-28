866a5cf1c47730b6d373c3eec65886cabfa81a48
zhangzp <zhangzp@goodcol.com>
2023-05-28 13:17:12 +0800

    去掉首页慢的头大的图片，换成纯色背景

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -166,13 +166,16 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
         // pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
 
+        //好像没有用，但是不写顶部就没有颜色
+        bodyBgImg: 'linear-gradient(180deg, #52ACFF 25%, #FFE32C 100%);',
+
         // bodyBgImg: '/img/bg01.jpg',
-        bodyBgImg: [
-            '/img/bg01_sm.jpg',
-            '/img/bg02_sm.jpg'
-        ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张。
-        bodyBgImgOpacity: 1, // body背景图透明度，选值 0.1~1.0, 默认0.5
-        bodyBgImgInterval: 300, // body多张背景图时的切换间隔, 默认15，单位s
+        // bodyBgImg: [
+        //     '/img/bg01_sm.jpg',
+        //     '/img/bg02_sm.jpg'
+        // ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张。
+        // bodyBgImgOpacity: 1, // body背景图透明度，选值 0.1~1.0, 默认0.5
+        // bodyBgImgInterval: 300, // body多张背景图时的切换间隔, 默认15，单位s
         // titleBadge: false, // 文章标题前的图标是否显示，默认true
         // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
         //   '图标地址1',
